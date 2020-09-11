dd9fa4abb4bb70cf8bd2d60f58c867265fb39ca1
Muhammad Anas Khan <anas.khan6960@gmail.com>
2020-09-11 15:08:06 +0500

    update the UI

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,7 +7,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { getMovies, deleteMovie } from "../services/fakeMovieService";
+import { getMovies } from "../services/fakeMovieService";
 
 const useStyles = makeStyles({
   table: {
@@ -18,7 +18,7 @@ const useStyles = makeStyles({
 const Movie = () => {
   const classes = useStyles();
   const [movies, setMovies] = useState("");
-
+  const { length } = movies;
   useEffect(() => {
     setMovies(getMovies());
   }, []);
@@ -28,52 +28,58 @@ const Movie = () => {
     setMovies(newMovies);
   };
 
+  if (movies.length === 0) {
+    return <p>There are no movies in the database.</p>;
+  }
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <strong>Title</strong>
-            </TableCell>
-            <TableCell align="right">
-              <strong>Genre</strong>
-            </TableCell>
-            <TableCell align="right">
-              <strong>Stock</strong>
-            </TableCell>
-            <TableCell align="right">
-              <strong>Rate</strong>
-            </TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
+    <React.Fragment>
+      <p>Showing {length} movies in the database</p>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <strong>Title</strong>
+              </TableCell>
+              <TableCell align="right">
+                <strong>Genre</strong>
+              </TableCell>
+              <TableCell align="right">
+                <strong>Stock</strong>
+              </TableCell>
+              <TableCell align="right">
+                <strong>Rate</strong>
+              </TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
 
-        {typeof movies === "object"
-          ? movies.map((row) => (
-              <TableBody key={row._id}>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    {row.title}
-                  </TableCell>
-                  <TableCell align="right">{row.genre.name}</TableCell>
-                  <TableCell align="right">{row.numberInStock}</TableCell>
-                  <TableCell align="right">{row.dailyRentalRate}</TableCell>
-                  <TableCell align="right">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => handleDelete(row._id)}
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            ))
-          : null}
-      </Table>
-    </TableContainer>
+          {typeof movies === "object"
+            ? movies.map((row) => (
+                <TableBody key={row._id}>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      {row.title}
+                    </TableCell>
+                    <TableCell align="right">{row.genre.name}</TableCell>
+                    <TableCell align="right">{row.numberInStock}</TableCell>
+                    <TableCell align="right">{row.dailyRentalRate}</TableCell>
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => handleDelete(row._id)}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              ))
+            : null}
+        </Table>
+      </TableContainer>
+    </React.Fragment>
   );
 };
 
