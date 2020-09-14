09830df01b562003e7246340ac712bd5059ddc14
Muhammad Anas Khan <anas.khan6960@gmail.com>
2020-09-15 00:26:40 +0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
 import Movie from "./components/Movie";
+import Navbar from "./components/layout/navbar";
+import { Switch, Route } from "react-router-dom";
+import Customers from "./components/Customer";
+import Rentals from "./components/Rentals";
+
 export default function App() {
-  return <Movie />;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/Rentals" component={Rentals} />
+        <Route path="/Customers" component={Customers} />
+        <Route path="/" component={Movie} />
+      </Switch>
+    </>
+  );
 }
---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Button, Typography } from "@material-ui/core";
+
+const Navbar = () => {
+  return (
+    <AppBar color="primary" position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ marginRight: 10 }}>
+          Vidly
+        </Typography>
+
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          <Button color="inherit">Movies</Button>
+        </Link>
+
+        <Link
+          to="/Customers"
+          style={{ color: "white", textDecoration: "none" }}
+        >
+          <Button color="inherit">Customer</Button>
+        </Link>
+        <Link to="/Rentals" style={{ color: "white", textDecoration: "none" }}>
+          <Button color="inherit">Rentals</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
